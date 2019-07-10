483700b50b6a6a858001b815696738f8e2479495
manish.kumar <manish.kr@velocis.co.in>
2019-07-10 16:40:39 +0530

    Permission

--- app/src/main/java/com/booking/hotelkaro/Activity/Splash.java
+++ app/src/main/java/com/booking/hotelkaro/Activity/Splash.java
@@ -1,14 +1,30 @@
 package com.booking.hotelkaro.Activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 
 import com.booking.hotelkaro.R;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.ACCESS_WIFI_STATE;
+import static android.Manifest.permission.CALL_PHONE;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.READ_PHONE_STATE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class Splash extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,30 +36,97 @@ public class Splash extends AppCompatActivity {
                     WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
         setContentView(R.layout.activity_splash);
+        requestPermission();
+        if (!checkPermission()) {
 
 
+        }
+        else {
 
-        new Thread(){
+            new Thread() {
 
-            @Override
-            public  void run() {
-
-
-                try{
+                @Override
+                public void run() {
 
 
-                    Thread.sleep(3000);
+                    try {
 
-                    startActivity(new Intent(Splash.this,Login.class));
-                    Splash.super.finish();
 
-                }catch (Exception e)
-                {
+                        Thread.sleep(3000);
 
+                        startActivity(new Intent(Splash.this, Login.class));
+                        Splash.super.finish();
+
+                    } catch (Exception e) {
+
+                    }
                 }
-            }
-        }.start();
+            }.start();
+
+        }
 
 
     }
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{ ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE);
+        /**/
+
+    }
+
+    private boolean checkPermission() {
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION);
+        int result2 = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_COARSE_LOCATION);
+
+        return result1 == PackageManager.PERMISSION_GRANTED && result2 == PackageManager.PERMISSION_GRANTED ;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+
+
+                    boolean finloc = grantResults[0] == getPackageManager().PERMISSION_GRANTED;
+                    boolean coarseloc = grantResults[1] == getPackageManager().PERMISSION_GRANTED;
+
+
+                    if (finloc && coarseloc) {
+
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                    }
+
+                    else {
+
+                        new Thread() {
+
+                            @Override
+                            public void run() {
+
+
+                                try {
+
+
+                                    Thread.sleep(3000);
+
+                                    startActivity(new Intent(Splash.this, Login.class));
+                                    Splash.super.finish();
+
+                                } catch (Exception e) {
+
+                                }
+                            }
+                        }.start();
+                    }
+                    finish();
+                }
+        }
+
+    }
+
+
+
 }
